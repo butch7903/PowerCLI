20d6dbd9b9e0e64dfb00c390fd1338d6d5b61453
butch7903 <31591018+butch7903@users.noreply.github.com>
2020-05-11 16:00:28 -0500

    Backup VCSA DB to File
    Use this script to backup the full VMware VCSA 6.5 or higher datebase. The script creates a current date named folder and places a backup copy of the database in that folder.
    
    This script will store all vCenter Username/Password and the Backup Location Password in an AES encrypted flat format to allow for future runnings of this script.
    
    Use this script with a Windows Task Scheduler or VMware Orchestrator to schedule/automate the backup process on a timely bases.

---
+++ Remove_Client_Plugins_From_VCSA.ps1
@@ -0,0 +1,229 @@
+<#
+    .NOTES
+	===========================================================================
+	Created by:		Russell Hamker
+	Date:			May 11, 2020
+	Version:		1.0
+	Twitter:		@butch7903
+	GitHub:			https://github.com/butch7903
+	===========================================================================
+
+	.SYNOPSIS
+		This script removes specific plugins from a VCSA. Usually this is needed for failed plugins that persist post an Upgrade
+
+	.DESCRIPTION
+		Use this script to remove plugins that are no longer needed
+
+	.NOTES
+		This script requires a VMware PowerCLI minimum version 11.4 or greater
+
+	.TROUBLESHOOTING
+		
+#>
+
+##Check if Modules are installed, if so load them, else install them
+if (Get-InstalledModule -Name VMware.PowerCLI -MinimumVersion 11.4) {
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	Write-Host "PowerShell Module VMware PowerCLI required minimum version was found previously installed"
+	Write-Host "Importing PowerShell Module VMware PowerCLI"
+	Import-Module -Name VMware.PowerCLI
+	Write-Host "Importing PowerShell Module VMware PowerCLI Completed"
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	#CLEAR
+} else {
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	Write-Host "PowerShell Module VMware PowerCLI does not exist"
+	Write-Host "Setting Micrsoft PowerShell Gallery as a Trusted Repository"
+	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+	Write-Host "Verifying that NuGet is at minimum version 2.8.5.201 to proceed with update"
+	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+	Write-Host "Uninstalling any older versions of the VMware PowerCLI Module"
+	Get-Module VMware.PowerCLI | Uninstall-Module -Force
+	Write-Host "Installing Newest version of VMware PowerCLI PowerShell Module"
+	Install-Module -Name VMware.PowerCLI -Scope AllUsers
+	Write-Host "Creating a Desktop shortcut to the VMware PowerCLI Module"
+	$AppLocation = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+	$Arguments = '-noe -c "Import-Module VMware.PowerCLI"'
+	$WshShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\VMware PowerCLI.lnk")
+	$Shortcut.TargetPath = $AppLocation
+	$Shortcut.Arguments = $Arguments
+	$ShortCut.Hotkey = "CTRL+SHIFT+V"
+	$Shortcut.IconLocation = "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe,1"
+	$Shortcut.Description ="Launch VMware PowerCLI"
+	$Shortcut.WorkingDirectory ="C:\"
+	$Shortcut.Save()
+	Write-Host "Shortcut Created"
+	Write-Host "You may use the CTRL+SHIFT+V method to open VMware PowerCLI"
+	Write-Host "Importing PowerShell Module VMware PowerCLI"
+	Import-Module -Name VMware.PowerCLI
+	Write-Host "PowerShell Module VMware PowerCLI Loaded"
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	#Clear
+}
+
+##Get Current Path
+$pwd = pwd
+
+##Document Start Time
+$STARTTIME = Get-Date -format "MMM-dd-yyyy HH-mm-ss"
+$STARTTIMESW = [Diagnostics.Stopwatch]::StartNew()
+
+##Select VCSA
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Select VCSA"
+$vCenter = Read-Host "Please provide the FQDN of your VCSA"
+Write-Host "VCSA Selected is $vCenter"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Get Date Info for Logging
+$LOGDATE = Get-Date -format "MMM-dd-yyyy_HH-mm"
+##Specify Log File Info
+$LOGFILENAME = "Log_" + $vCenter + "_" + $LOGDATE + ".txt"
+#Create Log Folder
+$LogFolder = $pwd.path+"\Log"
+If (Test-Path $LogFolder){
+	Write-Host "Log Directory Created. Continuing..."
+}Else{
+	New-Item $LogFolder -type directory
+}
+#Specify Log File
+$LOGFILE = $pwd.path+"\Log\"+$LOGFILENAME
+
+##Specify Export File Info
+$EXPORTFILENAME = "VMHostAudit_" + $vCenter + "_" + $LOGDATE + ".csv"
+#Create Export Folder
+$ExportFolder = $pwd.path+"\Export"
+If (Test-Path $ExportFolder){
+	Write-Host "Export Directory Created. Continuing..."
+}Else{
+	New-Item $ExportFolder -type directory
+}
+#Specify Log File
+$EXPORTFILE = $pwd.path+"\Export\"+$EXPORTFILENAME
+
+##Starting Logging
+Start-Transcript -path $LOGFILE -Append
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Logging Started"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Provide Credentials
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+If(!$MyCredential)
+{
+	Write-Host "Please Provide VCSA/NSXV Credentials"
+	$MyCredential = Get-Credential -Message "Please Provide VCSA Creds"
+	Write-Host "Credential UserName provided is:"$MyCredential.UserName
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from any open vCenter Sessions,
+#This can cause problems if there are any
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from any Open vCenter Sessions"
+TRY
+{Disconnect-VIServer * -Confirm:$false}
+CATCH
+{Write-Host "No Open vCenter Sessions found"}
+Write-Host "Disconnecting from any Open NSX Manager Sessions"
+if($DefaultNSXConnection.Server)
+{
+	Disconnect-NSXServer * -Confirm:$false
+}ELSE{
+	Write-Host "No Open NSX Server Sessions found"
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Connect to vCenter Server
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Connecting to vCenter $vCenter"
+$VISERVER = Connect-VIServer -server $vCenter -Credential $MyCredential
+Write-Host "Connected to vCenter "
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Review Plugins from VCSA
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "List VCSA Plugins from vCenter $vCenter"
+$em = Get-View ExtensionManager
+$em.ExtensionList | ft -Property Company,Key
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Remove Plugins from VCSA
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Removing Plugins from vCenter $vCenter"
+Write-Host "Removing Plugin com.vmware.vco (Orchestrator 6.0.0)"
+$em.UnregisterExtension("com.vmware.vco")
+$em.UpdateViewData()
+Write-Host "Listing Updated Post Results"
+$em = Get-View ExtensionManager
+$em.ExtensionList | ft -Property Company,Key
+Write-Host "Removing Plugin com.vmware.vcops (vROPs 6.5.0.0000000)"
+$em.UnregisterExtension("com.vmware.vcops")
+$em.UpdateViewData()
+Write-Host "Listing Updated Post Results"
+$em = Get-View ExtensionManager
+$em.ExtensionList | ft -Property Company,Key
+Write-Host "Checking for vRO Plugin Post"
+$em = Get-View ExtensionManager
+$em.ExtensionList | Where {$_.Key -like "com.vmware.vco"}
+Write-Host "Checking for vROPs Plugin Post"
+$em = Get-View ExtensionManager
+$em.ExtensionList | Where {$_.Key -like "com.vmware.vcops"}
+Write-Host "Completed Removing Plugins from vCenter $vCenter"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Document Script Total Run time
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+$STARTTIMESW.STOP()
+Write-Host "Total Script Time:"$STARTTIMESW.Elapsed.TotalMinutes"Minutes"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from vCenter
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from vCenter"
+disconnect-viserver $vCenter -confirm:$false
+IF ($NSXSERVER)
+{
+Write-Host "Disconnecting from NSX Manager"
+Disconnect-NSXServer -NSXServer $NSXSERVER -Confirm:$false
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Stopping Logging
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "All Processes Completed"
+Write-Host "Stopping Transcript"
+Stop-Transcript
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Script Completed
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Completed for $VCENTER"
+Write-Host "Press Enter to close this PowerShell Script"
+PAUSE
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+
