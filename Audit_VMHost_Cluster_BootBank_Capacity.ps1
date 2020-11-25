240e3e9cdd21e880299e22b0df53f66ff74b11b4
butch7903 <31591018+butch7903@users.noreply.github.com>
2020-11-25 10:17:07 -0600

    Create Audit_VMHost_Cluster_BootBank_Capacity.ps1

---
+++ Audit_VMHost_Cluster_BootBank_Capacity.ps1
@@ -0,0 +1,237 @@
+<#
+    .NOTES
+	===========================================================================
+	Created by:		Russell Hamker
+	Date:			November 25, 2020
+	Version:		1.0
+	Twitter:		@butch7903
+	GitHub:			https://github.com/butch7903
+	===========================================================================
+
+	.SYNOPSIS
+		This script will audit a VMHost's Boot Bank and report Total and Used space.
+
+	.DESCRIPTION
+		Use this script to audit the Boot Bank space to verify that it has enough
+		capacity for installing other VIBs including NSX-T
+	.NOTES
+		This script requires a VMware PowerCLI minimum version 11.4 or greater. 
+
+	.TROUBLESHOOTING
+		
+#>
+
+##Check if Modules are installed, if so load them, else install them
+if (Get-InstalledModule -Name VMware.PowerCLI -MinimumVersion 11.4) {
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	Write-Host "PowerShell Module VMware PowerCLI required minimum version was found previously installed"
+	Write-Host "Importing PowerShell Module VMware PowerCLI"
+	Import-Module -Name VMware.PowerCLI
+	Write-Host "Importing PowerShell Module VMware PowerCLI Completed"
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	#CLEAR
+} else {
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	Write-Host "PowerShell Module VMware PowerCLI does not exist"
+	Write-Host "Setting Micrsoft PowerShell Gallery as a Trusted Repository"
+	Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+	Write-Host "Verifying that NuGet is at minimum version 2.8.5.201 to proceed with update"
+	Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+	Write-Host "Uninstalling any older versions of the VMware PowerCLI Module"
+	Get-Module VMware.PowerCLI | Uninstall-Module -Force
+	Write-Host "Installing Newest version of VMware PowerCLI PowerShell Module"
+	Install-Module -Name VMware.PowerCLI -Scope AllUsers
+	Write-Host "Creating a Desktop shortcut to the VMware PowerCLI Module"
+	$AppLocation = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+	$Arguments = '-noe -c "Import-Module VMware.PowerCLI"'
+	$WshShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\VMware PowerCLI.lnk")
+	$Shortcut.TargetPath = $AppLocation
+	$Shortcut.Arguments = $Arguments
+	$ShortCut.Hotkey = "CTRL+SHIFT+V"
+	$Shortcut.IconLocation = "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe,1"
+	$Shortcut.Description ="Launch VMware PowerCLI"
+	$Shortcut.WorkingDirectory ="C:\"
+	$Shortcut.Save()
+	Write-Host "Shortcut Created"
+	Write-Host "You may use the CTRL+SHIFT+V method to open VMware PowerCLI"
+	Write-Host "Importing PowerShell Module VMware PowerCLI"
+	Import-Module -Name VMware.PowerCLI
+	Write-Host "PowerShell Module VMware PowerCLI Loaded"
+	Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+	#Clear
+}
+
+Import-Module VMware.PowerCLI
+
+##Get Current Path
+$pwd = pwd
+
+##Document Start Time
+$STARTTIME = Get-Date -format "MMM-dd-yyyy HH-mm-ss"
+$STARTTIMESW = [Diagnostics.Stopwatch]::StartNew()
+
+#Type in VCSA Name
+$VCSA = read-host "Please Provide VCSA FQDN"
+
+##Get Date Info for Logging
+$LOGDATE = Get-Date -format "MMM-dd-yyyy_HH-mm"
+##Specify Log File Info
+$LOGFILENAME = "Log_" + $VCSA + "_" + $LOGDATE + ".txt"
+#Create Log Folder
+$LogFolder = $pwd.path+"\Log"
+If (Test-Path $LogFolder){
+	Write-Host "Log Directory Created. Continuing..."
+}Else{
+	New-Item $LogFolder -type directory
+}
+#Specify Log File
+$LOGFILE = $pwd.path+"\Log\"+$LOGFILENAME
+
+##Starting Logging
+Start-Transcript -path $LOGFILE -Append
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Logging Started"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Provide Credentials
+CLS
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+If(!$MyCredential)
+{
+	Write-Host "Please Provide VCSA Administrator based Credentials for VCSA $VCSA"
+	$MyCredential = Get-Credential -Message "Please Provide VCSA Creds"
+	Write-Host "Credential UserName provided is:"$MyCredential.UserName
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from any open vCenter Sessions,
+#This can cause problems if there are any
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from any Open vCenter Sessions"
+TRY
+{Disconnect-VIServer * -Confirm:$false}
+CATCH
+{Write-Host "No Open vCenter Sessions found"}
+Write-Host "Disconnecting from any Open NSX Manager Sessions"
+if($DefaultNSXConnection.Server)
+{
+	Disconnect-NSXServer * -Confirm:$false
+}ELSE{
+	Write-Host "No Open NSX Server Sessions found"
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Connect to vCenter Server
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Connecting to vCenter $VCSA"
+$vCenter = Connect-VIServer -server $VCSA -Credential $MyCredential
+Write-Host "Connected to vCenter "
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Select CLUSTER
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+CLS
+Write-Host "Select Cluster on vCenter $VCSA"
+$CLUSTER = Get-Cluster | Sort Name
+$countCL = 0   
+Write-Host " " 
+Write-Host "Clusters: " 
+Write-Host " " 
+foreach($oC in $CLUSTER)
+{   
+	Write-Output "[$countCL] $oc" 
+	$countCL = $countCL+1  
+}
+Write-Host " "   
+$choice = Read-Host "On which Cluster do you want to look at?"
+$CLUSTER = Get-Cluster $CLUSTER[$choice]
+Write-Host "You have selected Cluster $CLUSTER on vCenter $VCSA"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+
+#Get VMHost List
+$VMHOSTLIST = $Cluster | Get-VMHost 
+
+$DETAILLIST = @()
+ForEach($VMHOST in $VMHOSTLIST)
+{
+	$TEMPLIST = "" | Select VMHOST, BootBankSizeMB, BootBankFreeMB, BootBankUUID
+	
+	#ESXCLI
+	$esxcli = Get-EsxCli -VMHost $VMHOST -V2
+		
+	#Get Boot Details
+	$BOOTDETAILS = $esxcli.system.boot.device.get.Invoke()
+
+	#Get Boot Bank Storage Details
+	$BOOTBANK = $esxcli.storage.filesystem.list.invoke() |Where {$_.UUID -eq $BOOTDETAILS.BootFilesystemUUID}
+	
+	#Convert Boot Bank Capacity to a Number (instead of a System String)
+	$BOOTBANKSIZE = $BOOTBANK.Size -as [Decimal] #[Decimal]::Parse($BOOTBANK.Size)
+	$BOOTBANKFREE = $BOOTBANK.Free -as [Decimal] #[Decimal]::Parse($BOOTBANK.Free)
+
+	#Convert Boot Bank #s to MB
+	$BOOTBANKSIZEMB = $BOOTBANKSIZE / 1MB
+	$BOOTBANKFREEMB = $BOOTBANKFREE / 1MB
+	#Write-Host "Bootbank Size is $BOOTBANKSIZEMB MB"
+	#Write-Host "Bootbank Free is $BOOTBANKFREEMB MB"
+	
+	$TEMPLIST.VMHOST = $VMHOST.NAME
+	$TEMPLIST.BootBankSizeMB = $BOOTBANKSIZEMB
+	$TEMPLIST.BootBankFreeMB = $BOOTBANKFREEMB
+	$TEMPLIST.BootBankUUID = $BOOTDETAILS.BootFilesystemUUID
+	$DETAILLIST += $TEMPLIST
+}
+Write-Output $DETAILLIST | ft
+#Write-Host $DETAILLIST
+
+
+##Document Script Total Run time
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+$STARTTIMESW.STOP()
+Write-Host "Total Script Time:"$STARTTIMESW.Elapsed.TotalMinutes"Minutes"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from vCenter
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from vCenter"
+disconnect-viserver $vCenter -confirm:$false
+IF ($NSXSERVER)
+{
+Write-Host "Disconnecting from NSX Manager"
+Disconnect-NSXServer -NSXServer $NSXSERVER -Confirm:$false
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Stopping Logging
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "All Processes Completed"
+Write-Host "Stopping Transcript"
+Stop-Transcript
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Script Completed
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Completed for $VCSA"
+Write-Host "Press Enter to close this PowerShell Script"
+PAUSE
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
