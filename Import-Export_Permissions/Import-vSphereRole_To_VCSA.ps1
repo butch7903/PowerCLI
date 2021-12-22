423e83cc5337f87201177776afef97dc54d49033
butch7903 <31591018+butch7903@users.noreply.github.com>
2021-12-22 17:02:23 -0600

    Create Import-vSphereRole_To_VCSA.ps1

---
+++ Import-Export_Permissions/Import-vSphereRole_To_VCSA.ps1
@@ -0,0 +1,156 @@
+<#
+    .NOTES
+	===========================================================================
+	Created by:		Russell Hamker
+	Date:			March 12,2021
+	Version:		1.1
+	Twitter:		@butch7903
+	GitHub:			https://github.com/butch7903
+	===========================================================================
+
+	.SYNOPSIS
+		This script Imports a Role to a VCSA from a file 
+
+	.DESCRIPTION
+		Use this script to import a role to a VCSA from a file
+
+	.TROUBLESHOOTING
+		
+#>
+
+##Get Current Path
+$pwd = pwd
+
+##Document Start Time
+$STARTTIME = Get-Date -format "MMM-dd-yyyy HH-mm-ss"
+$STARTTIMESW = [Diagnostics.Stopwatch]::StartNew()
+
+#Type in VCSA Name
+$VCSA = read-host "Please Provide VCSA FQDN"
+
+#Ttpe in Full Path to Role to Import
+$ROLEPATH = read-host "Please Provide the full file path to exported roles
+Example: R:\INFR\Scripts\Import-Export_Roles\Export\cvsmgtvc01w.tadu.dcn\Splunk App.role"
+
+##Provide Credentials
+#CLS
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+If(!$MyCredential)
+{
+	Write-Host "Please Provide VCSA Administrator based Credentials for VCSA $VCSA"
+	$MyCredential = Get-Credential -Message "Please Provide VCSA Creds"
+	Write-Host "Credential UserName provided is:"$MyCredential.UserName
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from any open vCenter Sessions,
+#This can cause problems if there are any
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from any Open vCenter Sessions"
+TRY
+{Disconnect-VIServer * -Confirm:$false}
+CATCH
+{Write-Host "No Open vCenter Sessions found"}
+Write-Host "Disconnecting from any Open NSX Manager Sessions"
+if($DefaultNSXConnection.Server)
+{
+	Disconnect-NSXServer * -Confirm:$false
+}ELSE{
+	Write-Host "No Open NSX Server Sessions found"
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Connect to vCenter Server
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Connecting to vCenter $VCSA"
+$vCenter = Connect-VIServer -server $VCSA -Credential $MyCredential
+Write-Host "Connected to vCenter "
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Get Date Info for Logging
+$LOGDATE = Get-Date -format "MMM-dd-yyyy_HH-mm"
+##Specify Log File Info
+$LOGFILENAME = "Log_" + $vCenter + "_" + $LOGDATE + ".txt"
+#Create Log Folder
+$LogFolder = $pwd.path+"\Log"
+If (Test-Path $LogFolder){
+	Write-Host "Log Directory Created. Continuing..."
+}Else{
+	New-Item $LogFolder -type directory
+}
+#Specify Log File
+$LOGFILE = $pwd.path+"\Log\"+$LOGFILENAME
+
+##Starting Logging
+Start-Transcript -path $LOGFILE -Append
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Logging Started"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+#Import Role
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+$ROLENAME = (Get-ChildItem $ROLEPATH).BaseName
+Write-Host "Importing Role $ROLENAME to $VCSA"
+#.\PowerShellTools\Import-vSphereRoles.ps1 -Path $ROLEPATH
+$ROLEDATA = Get-Content -Path $ROLEPATH | Where-Object {$_}
+New-Virole -Name $ROLENAME | Out-Null
+Write-Host "Created Role `"$ROLENAME`"" -BackgroundColor Green
+ForEach($PRIV in $ROLEDATA)
+{
+	Write-Host "Setting Permissions $PRIV on Role $ROLENAME" -ForegroundColor Yellow
+	Set-VIRole -Role $ROLENAME -AddPrivilege (Get-VIPrivilege -ID $PRIV) | Out-Null  
+}
+Write-Host "Completed Importing Role $ROLENAME"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Disconnect from vCenter
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Disconnecting from vCenter"
+disconnect-viserver $vCenter -confirm:$false
+IF ($NSXSERVER)
+{
+Write-Host "Disconnecting from NSX Manager"
+Disconnect-NSXServer -NSXServer $NSXSERVER -Confirm:$false
+}
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Document Script Total Run time
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+$STARTTIMESW.STOP()
+Write-Host "Total Script Time:"$STARTTIMESW.Elapsed.TotalMinutes"Minutes"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Stopping Logging
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "All Processes Completed"
+Write-Host "Stopping Transcript"
+Stop-Transcript
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+##Script Completed
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "Script Completed for Importing Role $ROLENAME to $VCSA" -ForegroundColor Green
+Write-Host "Press Enter to close this PowerShell Script"
+PAUSE
+Write-Host (Get-Date -format "MMM-dd-yyyy_HH-mm-ss")
+Write-Host "-----------------------------------------------------------------------------------------------------------------------"
+
+
+
